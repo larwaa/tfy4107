e082e04f24983e5d3ec8f7b63eeeb036212619cb
Lars Waage <46653859+larwaa@users.noreply.github.com>
2020-03-22 16:03:10 +0100

    Refactor: major code cleanup

--- cubicspline.py
+++ cubicspline.py
@@ -34,24 +34,16 @@ from scipy.interpolate import CubicSpline
 from scipy.constants import g as GRAVITY, pi as PI
 import math
 
-# Constants
-MASS_BALL = 30 / 1000 # kilograms
-MASS_FILLED_CIRCLE = 27 / 1000# kilograms
-MASS_EMPTY_CIRCLE = 13 / 1000 # kilograms
 
 # Horisontal avstand mellom festepunktene er 200 mm
 h = 200
 xfast=np.asarray([0,h,2*h,3*h,4*h,5*h,6*h,7*h])
 # Vi begrenser starthøyden (og samtidig den maksimale høyden) til
 # å ligge mellom 250 og 300 mm
-ymax = 300
 # yfast: tabell med 8 heltall mellom 50 og 300 (mm); representerer
 # høyden i de 8 festepunktene
-yfast=np.asarray(np.random.randint(50, ymax, size=8))
 # inttan: tabell med 7 verdier for (yfast[n+1]-yfast[n])/h (n=0..7); dvs
 # banens stigningstall beregnet med utgangspunkt i de 8 festepunktene.
-inttan = np.diff(yfast)/h
-attempts=1
 # while-løkken sjekker om en eller flere av de 3 betingelsene ovenfor
 # ikke er tilfredsstilt; i så fall velges nye festepunkter inntil
 # de 3 betingelsene er oppfylt
@@ -62,50 +54,53 @@ attempts=1
 
 # Omregning fra mm til m:
 xfast = xfast/1000
-yfast = yfast/1000
-
 yfast = np.asarray([0.297, 0.249, 0.235, 0.161, 0.175, 0.122, 0.193, 0.159])
-#Programmet beregner deretter de 7 tredjegradspolynomene, et
-#for hvert intervall mellom to nabofestepunkter.
+# Programmet beregner deretter de 7 tredjegradspolynomene, et
+# for hvert intervall mellom to nabofestepunkter.
 
-#Med scipy.interpolate-funksjonen CubicSpline:
+# Med scipy.interpolate-funksjonen CubicSpline:
 cs = CubicSpline(xfast, yfast, bc_type='natural')
 xmin = 0.000
 xmax = 1.401
 dx = 0.001
+
+## CONSTANTS
 x = np.arange(xmin, xmax, dx)
 Nx = len(x)
 y = cs(x)
 dy = cs(x,1)
 d2y = cs(x,2)
-
-#Plotting
-
-baneform = plt.figure('y(x)',figsize=(12,3))
-plt.plot(x,y,xfast,yfast,'*')
-plt.title('Banens form')
-plt.xlabel('$x$ (m)',fontsize=20)
-plt.ylabel('$y(x)$ (m)',fontsize=20)
-plt.ylim(0,0.350)
-plt.grid()
-plt.show()
-#baneform.savefig("baneform.pdf", bbox_inches='tight')
-#baneform.savefig("baneform.png", bbox_inches='tight')
+c = 2/5
+MASS_BALL = 30 / 1000 # kilograms
+MASS_FILLED_CIRCLE = 27 / 1000# kilograms
+MASS_EMPTY_CIRCLE = 13 / 1000 # kilograms
 
 
+# Plotting
+def track(x_data, y_data, x_fast, y_fast):
+	track_plot = plt.figure('y(x)',figsize=(12,3))
+	plt.plot(x_data, y_data, x_fast, y_fast, '*')
+	plt.title('Banens form')
+	plt.xlabel('$x$ (m)',fontsize=20)
+	plt.ylabel('$y(x)$ (m)',fontsize=20)
+	plt.ylim(0,0.350)
+	plt.grid()
+	plt.show()
+	track_plot.savefig("track_plot.pdf", bbox_inches='tight')
+
+
+# Helningsvinkel
 def beta():
 	return np.asarray([math.atan(derivative)/(2 * PI) * 180 for derivative in dy])
 
-# def v_x(x, c):
-	# return math.sqrt(2 * GRAVITY * (yfast[0] - y(x)) / (1 + c))
-
 
 # BETA FØRSTE PLOT
-plt.plot(x, beta())
-plt.grid()
-plt.xlabel('$x$ (m)', fontsize=20)
-plt.ylabel(r'$\beta$ (grader)', fontsize=20)
-plt.show()
+def beta_plot(x_data):
+	y_data = beta()
+	x_label = r'$x$ (m)'
+	y_label = r'$\beta$ (grader)'
+	name = 'beta_plot'
+	plot_data(x_data, y_data, x_label, y_label, name)
 # END BETA FØRSTE PLOT
 
 
@@ -114,32 +109,31 @@ def curve(d2y, dy):
 	return d2y/(1 + dy ** 2) ** (3/2)
 
 
-plt.plot(x, curve(d2y, dy))
-plt.grid()
-plt.xlabel(r'$x$(m)', fontsize=20)
-plt.ylabel(r'$\kappa$(x) (1/m)')
-plt.show()
-# END KRUMNINGSPLOT
+def curve_plot(x_data, curve_func):
+	y_data = curve_func(d2y, dy)
+	x_label = r'$x$(m)'
+	y_label = r'$\kappa$(x) (1/m)'
+	name = "curve_plot"
+	plot_data(x_data, y_data, x_label, y_label, name)
+# END KRUMNINGSPLOTs
 
 
-def v(y, x, c):
-	a = []
-	for instant_y in y:
-		a.append(instant_v(y, instant_y, c))
-	return np.asarray(a)
+def v(y_data, c):
+	result = []
+	for instant_y in y_data:
+		result.append(instant_v(y_data, instant_y, c))
+	return np.asarray(result)
 
 
 def instant_v(y, y_x, c):
 	return math.sqrt(2 * GRAVITY * (y[0] - y_x) / (1 + c))
 
 
-print(GRAVITY)
-c = 2/5
-plt.plot(x, v(y, x, c))
-plt.grid()
-plt.xlabel(r'$x$ (m)', fontsize=20)
-plt.ylabel(r'$v(x)$ m/s', fontsize=20)
-plt.show()
+def v_plot(x_data, y_data, c, v_func):
+	y_data = v_func(y_data, x_data, c)
+	x_label = r'$x$ (m)'
+	y_label = r'$v(x)$ m/s'
+	plot_data(x_data, y_data, x_label, y_label)
 
 
 def centripetal_acceleration(velocity, curve):
@@ -149,42 +143,49 @@ def centripetal_acceleration(velocity, curve):
 	return np.asarray(result)
 
 
-def normal_force(mass, beta, acceleration):
+def normal_force(mass, acceleration):
 	result = []
+	beta_data = beta()
 	for i in range(len(acceleration)):
-		result.append(mass * (GRAVITY * math.cos(beta[i] * 2 * PI / 180) + acceleration[i]))
+		result.append(mass * (GRAVITY * math.cos(beta_data[i] * 2 * PI / 180) + acceleration[i]))
 	return np.asarray(result)
 
 
 # BEGIN NORMAL FORCE PLOT
-velocity = v(y, x, c)
-curve = curve(d2y, dy)
-acceleration = centripetal_acceleration(velocity, curve)
-normal = normal_force(MASS_BALL, beta(), acceleration)
-plt.plot(x, normal)
-plt.grid()
-plt.xlabel(r'$x$ (m)', fontsize=20)
-plt.ylabel(r'$N(X)$, N', fontsize=20)
-plt.show()
+def normal_force_plot(x_data, y_data, mass_object, c):
+	velocity = v(y_data, x_data, c)
+	curve_data = curve(d2y, dy)
+	acceleration = centripetal_acceleration(velocity, curve_data)
+	normal = normal_force(mass=mass_object, acceleration=acceleration)
+
+	x_label = r'$x$ (m)'
+	y_label = r'$N(X)$, N'
+	name = "normal_plot"
+	plot_data(x_data, normal, x_label, y_label, name)
 
 
-def friction(c, mass, beta):
+def friction(c, mass):
 	result = []
-	for value in beta:
+	for value in beta():
 		result.append(c * mass * GRAVITY * math.sin(value * 2 * PI / 180) / (1 + c))
 	return np.asarray(result)
 
 
-def horizontal_velocity(beta, velocity):
+def horizontal_velocity(velocity):
 	v_avg = [0]
-	for i in range(1, len(beta)):
-		v_avg.append(1/2 * (v_avg[i - 1] + instant_horizontal_velocity(beta[i], velocity[i])))
+	beta_data = beta()
+	print(len(beta_data))
+	print(len(velocity))
+	for i in range(1, len(beta_data)):
+		print(i)
+
+		v_avg.append(1/2 * (v_avg[i - 1] + instant_horizontal_velocity(beta_data[i], velocity[i])))
 	return np.asarray(v_avg)
 
 
-def time(beta, velocity):
+def time(velocity):
 	time = [0]
-	v_avg = horizontal_velocity(beta, velocity)
+	v_avg = horizontal_velocity(velocity)
 	for i in range(1, 1401):
 		time.append(((1/1000) / v_avg[i]) + time[i - 1])
 	return np.asarray(time)
@@ -194,32 +195,63 @@ def instant_horizontal_velocity(beta, velocity):
 	return velocity * math.cos(beta * 2 * PI / 180)
 
 
-plt.plot(x, friction(c, MASS_BALL, beta()))
-plt.grid()
-plt.xlabel(r'$x$ (m)', fontsize=20)
-plt.ylabel(r'$friction$', fontsize=20)
-plt.show()
-beta = beta()
-norm = normal_force(MASS_BALL, beta, acceleration)
-friction = friction(c, MASS_BALL, beta)
-
-friction_normal = np.asarray([abs(friction[i]/norm[i]) for i in range(len(friction))])
-plt.plot(x, friction_normal)
-plt.grid()
-plt.xlabel(r'$x$ (m)', fontsize=20)
-plt.ylabel(r'$|f/N|$', fontsize=20)
-plt.gca().set_ylim([0, 0.4])
-plt.show()
+def friction_plot(x_data, mass_object, c):
+	y_data = friction(c=c, mass=mass_object)
+	x_label = r'$x$ (m)'
+	y_label = r'$friction$'
+	name = "friction_plot"
+	plot_data(x_data, y_data, x_label, y_label, name)
 
 
-plt.plot(time(beta=beta, velocity=velocity), x)
-plt.grid()
-plt.xlabel(r'$t$ (s)', fontsize=20)
-plt.ylabel(r'$x$ (m)', fontsize=20)
-plt.show()
+def friction_normal_plot(x_data, y_data, mass_object, c):
+	acceleration = centripetal_acceleration(velocity=v(y_data, c), curve=curve(d2y, dy))
+	norm = normal_force(mass=mass_object, acceleration=acceleration)
+	friction_data = friction(c=c, mass=mass_object)
+	y_data = np.asarray([abs(friction_data[i] / norm[i]) for i in range(len(friction_data))])
 
-plt.plot(time(beta=beta, velocity=velocity), horizontal_velocity(beta=beta, velocity=velocity))
-plt.grid()
-plt.xlabel(r'$t$ (s)', fontsize=20)
-plt.ylabel(r'$v(t)$ (m/s)', fontsize=20)
-plt.show()
+	x_label = r'$x$ (m)'
+	y_label = r'$|f/N|$'
+	name = "friction_normal_plot"
+	y_lim = [0, 0.4]
+	plot_data(x_data, y_data, x_label, y_label, name, y_lim=y_lim)
+
+
+def velocity_horizontal_displacement_plot(x_data, y_data, c):
+	temp = x_data
+	x_data = time(velocity=v(y_data, x_data, c))
+	y_data = temp
+	x_label = r'$t$ (s)'
+	y_label = r'$x$ (m)'
+	name = "velcity_horizontal_displacement_plot"
+	plot_data(x_data, y_data, x_label, y_label, name)
+
+
+def velocity_time_plot(y_data, c):
+	velocity = v(y_data, c)
+	x_data = time(velocity)
+	y_data = horizontal_velocity(velocity)
+	x_label = r'$t$ (s)'
+	y_label = r'$v(t)$ (m/s)'
+	name = "velocity_time_plot"
+	plot_data(x_data, y_data, x_label, y_label, name)
+
+
+def plot_data(x_data, y_data, x_label, y_label, name="", y_lim=None):
+	plot = plt.figure(x_label, figsize=(12, 3))
+	plt.plot(x_data, y_data)
+	plt.grid()
+	plt.xlabel(x_label, fontsize=20)
+	plt.ylabel(y_label, fontsize=20)
+
+	if y_lim is not None:
+		plt.gca().set_ylim(y_lim)
+
+	plt.show()
+
+	if name != "":
+		plot.savefig(f"{name}.pdf", bbox_inches="tight")
+
+
+track(x, y, xfast, yfast)
+velocity_time_plot(y, c)
+friction_normal_plot(x_data=x, y_data=y, mass_object=MASS_BALL, c=c)
